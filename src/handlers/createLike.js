a0ae7ca4d770cdedc7e387f289b86364b3f9c6b2
javahyang <javahyang@gmail.com>
2021-01-22 23:54:59 +0900

    like 객체 기본골격

--- src/handlers/createLike.js
+++ src/handlers/createLike.js
@@ -1,10 +1,16 @@
 async function createLike(event, context) {
-  // context : meta 정보 등. user_id 등을 담기도 한다.
-  // custom 데이터는 event, context 모두에 담을 수 있다
+  const { title, type } = JSON.parse(event.body);
+  const now = new Date();
+
+  const like = {
+    title,
+    type,
+    createdAt: now.toISOString(), //ISO 타입이 데이터베이스에 날짜 넣는 기본 형식
+  };
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }) // 람다에서는 반드시 JSON.stringify 버전으로 보내야한다. 아니면 에러!
+    statusCode: 201,
+    body: JSON.stringify(like)
   }
 }
 
